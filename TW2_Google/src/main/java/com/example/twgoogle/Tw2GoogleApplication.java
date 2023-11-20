2718d5c43aaa15ec39d28229134cde75594becfc
Aintza03 <115790076+Javi9Deusto@users.noreply.github.com>
2023-11-20 10:46:12 +0100

    v1

---
+++ TW2_Google/src/main/java/com/example/twgoogle/Tw2GoogleApplication.java
@@ -0,0 +1,13 @@
+package com.example.twgoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tw2GoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tw2GoogleApplication.class, args);
+	}
+
+}
---
+++ TW2_Google/src/test/java/com/example/twgoogle/Tw2GoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twgoogle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tw2GoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
